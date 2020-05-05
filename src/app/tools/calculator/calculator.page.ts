c7498c485fc681f7025dfffddcab65e1d4e62d40
Syed Salman Manzoor <immasalman@gmail.com>
2020-05-05 13:51:24 +0500

    calc changes

--- src/app/tools/calculator/calculator.page.ts
+++ src/app/tools/calculator/calculator.page.ts
@@ -12,6 +12,11 @@ export class CalculatorPage implements OnInit {
   width: number;
   length: number;
   height: number;
+  weight: number;
+  qty: number;
+  volume: any;
+  tweight: any;
+  vweight: any;
 
   constructor() { }
 
@@ -22,5 +27,8 @@ export class CalculatorPage implements OnInit {
   calculateCBM() {
     this.cbm = (this.width * this.length * this.height) / 1000000;
     console.log(this.cbm);
+    this.volume = (this.cbm * this.qty).toFixed(5);
+    this.tweight = this.weight * this.qty;
+    this.vweight = (this.length * this.height * this.qty * this.width) / 5000;
   }
 }
