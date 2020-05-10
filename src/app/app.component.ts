805191e60a45d0aa82228517b275ad839045f8ad
Syed Salman Manzoor <immasalman@gmail.com>
2020-05-10 23:55:49 +0500

    delay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -81,7 +81,7 @@ export class AppComponent implements OnInit {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
 
-      timer(3000).subscribe(() => this.showSplash = false); // <-- hide animation after 3s
+      timer(5000).subscribe(() => this.showSplash = false); // <-- hide animation after 3s
 
     });
   }
